45289d6e4834597c680b780a0f49fc8305839757
Catalina Restrepo Salgado <rposalgadoc@gmail.com>
2022-09-03 12:35:34 -0500

    Correcciones al código
    He realizado algunas modificaciones en las clases Asignatura y Grupo, arreglando la recepción de parámetros y la ejecución de algunos métodos con el fin de obtener el resultado esperado.

--- classroom/asignatura.py
+++ classroom/asignatura.py
@@ -1,5 +1,12 @@
+from pydoc import describe
+
+
 class Asignatura:
 
     def __init__(self, nombre, salon="remoto"):
         self._nombre = nombre
         self._salon = salon
+    
+    def __str__(self):
+        descripcion = self._nombre + " " + self._salon
+        return descripcion
